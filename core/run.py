d15cdcaf39a0f1946f49ee502dd64d45752f55ec
Jiangqiu Shen <jshen2@mtu.edu>
2020-05-25 08:22:12 -0400

    add statistics

---
+++ core/run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from multiprocessing import Pool
+import subprocess
+
+# cnfs=["ASG_96_len112_known_last12_2.cnf","eqspwtrc16bparrc16.cnf","Mickey_out250_known_last146_0.cnf","toughsat_23bits_0.cnf","b1904P1-8x8c6h5SAT.cnf","files.txt,MM-23-2-2-2-2-3.cnf","toughsat_25bits_1.cnf","b1904P3-8x8c11h0SAT.cnf","Grain_no_init_ver1_out200_known_last104_0.cnf","QuasiGroup-4-12_c18.cnf","toughsat_28bits_0.cnf","Bibd-sc-10-03-08_c18.cnf","Haystacks-ext-12_c18.cnf","sha1r17m145ABCD.cnf","toughsat_30bits_0.cnf","build_png.py,hcp_bij16_16.cnf","sha1r17m148ABCD_p.cnf","Trivium_no_init_out350_known_last142_1.cnf","hcp_CP20_20.cnf","sha1r17m72a.cnf","eqsparcl10bpwtrc10.cnf","hcp_CP24_24.cnf","size_4_4_4_i0418_r8.cnf","eqspdtlf14bpwtrc14.cnf","knight_20.cnf","size_5_5_5_i003_r12.cnf"]
+cnfs = ["ASG_96_len112_known_last12_2.cnf", "eqspwtrc16bparrc16.cnf",
+        "Mickey_out250_known_last146_0.cnf", "toughsat_23bits_0.cnf", "b1904P1-8x8c6h5SAT.cnf"]
+
+
+def run_task(command):
+    subprocess.run(command, shell=True)
+
+
+commands = ["/home/sjq/Downloads/minisat/core/minisat_release ~/cnfs/"+c +
+            " 2>&1 |tail -n 1000 > result_"+c+".txt" for c in cnfs]
+with Pool(14) as p:
+    p.map(run_task, commands)
