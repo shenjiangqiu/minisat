390759056b479d1936a4c2dcb1046c1509b9986f
Jiangqiu Shen <jshen2@mtu.edu>
2022-03-15 16:16:06 -0400

    update

--- test/occ_test.cc
+++ test/occ_test.cc
@@ -1,5 +1,5 @@
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include "mtl/Vec.h"
 #include "core/Solver.h"
 #include "core/SolverTypes.h"
--- test/solver_test.cc
+++ test/solver_test.cc
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include "mtl/Vec.h"
 #include "core/Solver.h"
 #include "core/SolverTypes.h"
